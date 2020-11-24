c8f77834ded7007ae39ab2e59d0faed18154635e
noname <48761044+noname0310@users.noreply.github.com>
2020-11-24 12:51:37 +0900

    upload code

---
+++ POHAlloc/POHAlloc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+unsafe
+{
+    var foo = Malloc<int>(sizeof(int)).value;
+    *foo = 10;
+    Console.WriteLine(foo->ToString());
+
+    //safe
+    var bar = Malloc<int>(sizeof(int));
+    *bar.value = 10;
+    Console.WriteLine(bar.value->ToString());
+}
+
+unsafe static UnManaged<T> Malloc<T>(int size) where T : unmanaged
+{
+    byte[] pinnedArr = GC.AllocateUninitializedArray<byte>(size, true);
+    fixed (byte* ptr = pinnedArr) { return new((T*)ptr, pinnedArr); }
+}
+
+unsafe struct UnManaged<T> where T : unmanaged
+{
+    public T* value;
+    public byte[] src;
+
+    public UnManaged(T* value, byte[] src)
+    {
+        this.value = value;
+        this.src = src;
+    }
+}
